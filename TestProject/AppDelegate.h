797c40449efaea775f597e41fbb5aa844d34b149
舒永超 <shuyongchao@shuyongchaodemac-mini.local>
2016-03-18 09:18:39 +0800

    Initial Commit

---
+++ TestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestProject
+//
+//  Created by 舒永超 on 16/3/18.
+//  Copyright © 2016年 舒永超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestProject
+//
+//  Created by 舒永超 on 16/3/18.
+//  Copyright © 2016年 舒永超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
